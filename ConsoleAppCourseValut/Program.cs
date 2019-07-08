bfb57b6b9ab776d2009549dfbf9321ebc3a58339
WWWNasty <nas.grischenk@yandex.ru>
2019-07-08 14:25:17 +0300

    Add project files.

---
+++ ConsoleAppCourseValut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCourseValut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
